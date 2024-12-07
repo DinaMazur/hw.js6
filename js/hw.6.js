fbec7b52b0b0175b0a832f29d953d968f886a726
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-12-07 12:59:00 +0200

    Add files via upload

---
+++ js/hw.6.js
@@ -0,0 +1,68 @@
+// let counter = 0;
+// while (counter <= 10) {
+//     console.log("counter:" , counter);
+//     counter +=1
+// }
+
+// const min = 2;
+// const max = 20;
+// let total = 0;
+// for (let i = min; i <= max; i += 1){
+//     if (i % 2 !== 0 ) {
+//         continue;
+//     }
+//     console.log("непарне число:", i);
+// }
+
+
+
+// for (let i = 1; i <= 10; i += 1) {
+//     console.log(`7 x ${i} = ${7 * i}`);
+// }
+
+
+
+
+// const array = [1, 2, 3, 4, 5];
+// let y = 0;
+
+// while (y < array.length) {
+//     console.log(array[y]);
+//     y++
+// }
+
+
+
+
+// let things = ["1", "2", "3", "4", "5", "6", "7"];
+// for (let i = 0; i < 10; i += 1 ){
+//     if (i === 7) {
+//         break;
+//     }
+// }
+
+
+// let n = 15;
+
+// for (let k = 1; k <= 20; k++) {
+//     if (k >= n) {
+//         break;
+//     }
+//     console.log(k);
+// }
+
+
+
+// let d = 1;
+
+// while (d <= 20) {
+//     if (d % 3 === 0) {
+//         d++;
+//         continue;
+//     }
+//     console.log(d);
+//     d++
+// }
+
+
+
